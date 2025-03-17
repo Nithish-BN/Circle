68b74d2a547dc082deff566f39be9fddb91319ec
ViratPKGupta15 <vvce23cseaiml0064@vvce.ac.in>
2025-03-17 14:50:10 +0530

    First Commit

---
+++ circle.java
@@ -0,0 +1,15 @@
+public class Circle{
+	public static void main (String [] args){
+	Scanner sc = new Scanner(System.in);
+         System.out.println("Enter The Radius :");
+	 int r = sc.nextInt();
+
+	 int circle = .5 * r * r;
+
+	 System.out.println("Area : " + circle);
+
+
+
+
+	}
+}
